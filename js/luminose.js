f63fb193869533dc054a66f0dfcd669874080496
Florent Jaouali <florent.jaouali@gmail.com>
2021-01-14 11:25:31 +0100

    Coherence cardiaque

--- js/luminose.js
+++ js/luminose.js
@@ -45,3 +45,52 @@ class BulmaModal {
     this.elem.addEventListener(event, callback)
   }
 }
+
+// Main App
+var App = {
+
+  _config: {
+
+  },
+
+  run: function() {
+      this._enableButtonMentionsLegales();
+      this._loadRespirationControls();
+  },
+
+  _enableButtonMentionsLegales: function() {
+    var button = document.querySelector('#bt-mentions-legales');
+    if (button !== null) {
+      var modal = new BulmaModal("#md-mentions-legales");
+      button.addEventListener("click", function () {
+      	modal.show();
+      });
+    }
+  },
+
+  _loadRespirationControls: function() {
+    var buttonStart = document.querySelector('#bt-respiration-start'),
+        buttonStop  = document.querySelector('#bt-respiration-stop'),
+        progressbar = document.querySelector('#pg-respiration');
+
+    if ((buttonStart !== null) && (buttonStop !== null) && (progressbar !== null)) {
+      buttonStart.addEventListener('click', function () {
+        progressbar.classList.toggle("paused");
+        buttonStart.classList.toggle("paused");
+        buttonStop.classList.toggle("running");
+      });
+      buttonStop.addEventListener('click', function () {
+        progressbar.classList.remove("loader__bar");
+        progressbar.classList.remove("paused");
+        buttonStop.classList.remove("running");
+        void progressbar.offsetWidth;
+        progressbar.classList.add("loader__bar");
+        progressbar.classList.add("paused");
+        buttonStart.classList.add("paused");
+      });
+      var before = window.getComputedStyle(progressbar, '::before');
+      var iterationsCount = before.getPropertyValue('animation-iteration-count');
+      console.log(iterationsCount);
+    }
+  }
+}
