fccdb1d1f6edcfb772e7ab70dad1b8619267adfc
florent.jaouali@gmail.com <florent.jaouali@gmail.com>
2021-01-19 15:45:11 +0100

    Adding sound

--- js/luminose.js
+++ js/luminose.js
@@ -51,7 +51,7 @@ var App = {
   run: function() {
     this._setupViewport();
     this._enableButtonMentionsLegales();
-    this._loadRespirationControls();
+    this.respiration.run();
   },
 
   _setupViewport: function() {
@@ -75,88 +75,266 @@ var App = {
     }
   },
 
-  _loadRespirationControls: function() {
-    var buttonStart = document.querySelector('#bt-respiration-start'),
-        buttonStop  = document.querySelector('#bt-respiration-stop'),
-        buttonConfig  = document.querySelector('#bt-config'),
-        dropdownConfig  = document.querySelector('#dp-config'),
-        tracker = document.querySelector('#pg-track'),
-        progressbarTracker = document.querySelector('#pg-track .completed'),
-        progressbar = document.querySelector('#pg-respiration');
+  respiration: {
 
-    if ((buttonStart !== null) && (buttonStop !== null) && (buttonConfig !== null) && (dropdownConfig !== null) && (progressbar !== null)) {
-      buttonStart.addEventListener('click', function () {
-        progressbar.classList.toggle("paused");
-        progressbarTracker.classList.toggle("running");
-        buttonStart.classList.toggle("paused");
-        buttonStop.classList.toggle("running");
-        buttonConfig.classList.toggle("running");
-      });
-      buttonStop.addEventListener('click', function () {
-        progressbar.classList.remove("loader__bar", "paused");
-        progressbarTracker.classList.remove("running", "completed");
-        buttonStop.classList.remove("running");
-        buttonConfig.classList.remove("running");
-        iterationsCounter = 0;
-        void progressbar.offsetWidth;
-        progressbar.classList.add("loader__bar", "paused");
-        progressbarTracker.classList.add("completed");
-        buttonStart.classList.add("paused");
-      });
-      buttonConfig.addEventListener('click', function () {
-        buttonConfig.classList.toggle("is-active");
-        dropdownConfig.classList.toggle("is-active");
-      });
-      dropdownConfig.querySelectorAll('.set-speed span.values span').forEach(function(btSpeed) {
-        btSpeed.addEventListener('click', function () {
-          var value = btSpeed.innerText;
-          progressbar.classList.remove("speed3sec", "speed4sec", "speed5sec", "speed6sec", "speed7sec");
-          progressbar.classList.add("speed" + value + "sec");
-          dropdownConfig.querySelector('.set-speed span.is-active').classList.remove("is-active");
-          btSpeed.classList.add("is-active");
+    _runningTimer: null,
+    _runningTimer2: null,
+
+    run: function() {
+      this.animations._load(this);
+      this.sounds._load(this);
+      this.buttons._load(this);
+    },
+
+    startPause:function() {
+      var that = this;
+      if (that.animations.stopped) {
+        that.animations.stopped = false;
+        that.sounds._setupSoundTimers();
+        that.play();
+      } else {
+        if (that.animations.paused) {
+          that.play();
+        } else {
+          that.pause();
+        }
+      }
+    },
+
+    play:function() {
+      var that = this;
+      that.animations.main.classList.remove("paused");
+      that.animations.progress.classList.add("running");
+      that.buttons.play.classList.remove("paused");
+      that.buttons.stop.classList.add("running");
+      that.buttons.config.classList.add("running");
+      that.sounds.play();
+      that.animations.paused = false;
+    },
+
+    pause:function() {
+      var that = this;
+      that.animations.main.classList.add("paused");
+      that.animations.progress.classList.remove("running");
+      that.buttons.play.classList.add("paused");
+      that.buttons.stop.classList.remove("running");
+      that.buttons.config.classList.remove("running");
+      that.sounds.pause();
+      that.animations.paused = true;
+    },
+
+    stop:function() {
+      var that = this;
+      that.sounds.stop();
+      that.animations.main.classList.remove("loader__bar", "paused");
+      that.animations.progress.classList.remove("running", "completed");
+      that.buttons.stop.classList.remove("running");
+      that.buttons.config.classList.remove("running");
+      void that.animations.main.offsetWidth;
+      that.animations.main.classList.add("loader__bar", "paused");
+      that.animations.progress.classList.add("completed");
+      that.buttons.play.classList.add("paused");
+      that.animations.stopped = true;
+      that.animations.paused = true;
+    },
+
+    setRecurringTimer: function(callback, delay, initialStartDelay) {
+        var timerId, start, remaining = delay;
+        if (initialStartDelay >= 0) {
+          remaining = initialStartDelay;
+        }
+
+        this.pause = function() {
+            window.clearTimeout(timerId);
+            remaining -= new Date() - start;
+        };
+
+        this.stop = function() {
+          window.clearTimeout(timerId);
+        }
+
+        var resume = function() {
+            start = new Date();
+            timerId = window.setTimeout(function() {
+                remaining = delay;
+                resume();
+                callback();
+            }, remaining);
+        };
+
+        this.resume = resume;
+        this.resume();
+    },
+
+    animations: {
+      main: null,
+      progress: null,
+      progressContainer: null,
+      stopped: true,
+      paused: true,
+      _parent: null,
+
+      _load: function(caller) {
+        this._parent = caller;
+        this.progressContainer = document.querySelector('#pg-track'),
+        this.progress = document.querySelector('#pg-track .completed'),
+        this.main = document.querySelector('#pg-respiration');
+
+        var that = this;
+
+        this.main.addEventListener('animationend', function() {
+          console.log('end');
+          that._parent.stop();
         });
-      })
-      progressbar.addEventListener('animationend', function() {
-        buttonStop.click();
-      });
 
-      var bt5min = dropdownConfig.querySelector('#length5min'),
-          bt10min = dropdownConfig.querySelector('#length10min'),
-          btinfinite = dropdownConfig.querySelector('#lengthinfinite');
-      bt5min.addEventListener('click', function () {
-        if (!bt5min.classList.contains("is-active")) {
-          progressbar.classList.remove("length10min", "lengthinfinite");
-          progressbarTracker.classList.remove("length10min", "lengthinfinite");
-          progressbar.classList.add("length5min");
-          progressbarTracker.classList.add("length5min");
-          tracker.style.display = "block";
-          dropdownConfig.querySelector('.set-length span.is-active').classList.remove("is-active");
-          bt5min.classList.add("is-active");
-        }
-      });
-      bt10min.addEventListener('click', function () {
-        if (!bt10min.classList.contains("is-active")) {
-          progressbar.classList.remove("length5min", "lengthinfinite");
-          progressbarTracker.classList.remove("length5min", "lengthinfinite");
-          progressbar.classList.add("length10min");
-          progressbarTracker.classList.add("length10min");
-          tracker.style.display = "block";
-          dropdownConfig.querySelector('.set-length span.is-active').classList.remove("is-active");
-          bt10min.classList.add("is-active");
-        }
-      });
-      btinfinite.addEventListener('click', function () {
-        if (!btinfinite.classList.contains("is-active")) {
-          progressbar.classList.remove("length5min", "length10min");
-          progressbarTracker.classList.remove("length5min", "length10min");
-          progressbar.classList.add("lengthinfinite");
-          progressbarTracker.classList.add("lengthinfinite")
-          tracker.style.display = "none";
-          dropdownConfig.querySelector('.set-length span.is-active').classList.remove("is-active");
-          btinfinite.classList.add("is-active");
-        }
-      });
+        // this.main.addEventListener('animationiteration', function() {
+        //   console.log('iteration');
+        //   that._parent._onIteration();
+        // });
+      }
+    },
 
+    buttons: {
+      play: null,
+      stop: null,
+      config: null,
+      configPanel: null,
+      _parent: null,
+
+      _load: function(caller) {
+        this._parent = caller;
+
+        if (this._parent.animations.main !== null) {
+          this.play         = document.querySelector('#bt-respiration-start');
+          this.stop         = document.querySelector('#bt-respiration-stop');
+          this.config       = document.querySelector('#bt-config');
+          this.configPanel  = document.querySelector('#dp-config');
+
+          if ((this.play !== null) && (this.stop !== null) && (this.config !== null) && (this.configPanel !== null)) {
+
+            var that = this,
+                bt5min = that.configPanel.querySelector('#length5min'),
+                bt10min = that.configPanel.querySelector('#length10min'),
+                btinfinite = that.configPanel.querySelector('#lengthinfinite');
+
+            that.play.addEventListener('click', function () {
+              that._parent.startPause();
+            });
+
+            that.stop.addEventListener('click', function () {
+              that._parent.stop();
+            });
+
+            that.config.addEventListener('click', function () {
+              that.config.classList.toggle("is-active");
+              that.configPanel.classList.toggle("is-active");
+            });
+
+            that.configPanel.querySelectorAll('.set-speed span.values span').forEach(function(btSpeed) {
+              btSpeed.addEventListener('click', function () {
+                ////////
+                ///////
+                //////
+                /////
+                //// A revoir avec le son + ajouter [mute] en config
+                var value = btSpeed.innerText;
+                that._parent.animations.main.classList.remove("speed3sec", "speed4sec", "speed5sec", "speed6sec", "speed7sec");
+                that._parent.animations.main.classList.add("speed" + value + "sec");
+                that.configPanel.querySelector('.set-speed span.is-active').classList.remove("is-active");
+                btSpeed.classList.add("is-active");
+              });
+            });
+
+            bt5min.addEventListener('click', function () {
+              if (!bt5min.classList.contains("is-active")) {
+                that._parent.animations.main.classList.remove("length10min", "lengthinfinite");
+                that._parent.animations.progress.classList.remove("length10min", "lengthinfinite");
+                that._parent.animations.main.classList.add("length5min");
+                that._parent.animations.progress.classList.add("length5min");
+                that._parent.animations.progressContainer.style.display = "block";
+                that.configPanel.querySelector('.set-length span.is-active').classList.remove("is-active");
+                bt5min.classList.add("is-active");
+              }
+            });
+
+            bt10min.addEventListener('click', function () {
+              if (!bt10min.classList.contains("is-active")) {
+                that._parent.animations.main.classList.remove("length5min", "lengthinfinite");
+                that._parent.animations.progress.classList.remove("length5min", "lengthinfinite");
+                that._parent.animations.main.classList.add("length10min");
+                that._parent.animations.progress.classList.add("length10min");
+                that._parent.animations.progressContainer.style.display = "block";
+                that.configPanel.querySelector('.set-length span.is-active').classList.remove("is-active");
+                bt10min.classList.add("is-active");
+              }
+            });
+
+            btinfinite.addEventListener('click', function () {
+              if (!btinfinite.classList.contains("is-active")) {
+                that._parent.animations.main.classList.remove("length5min", "length10min");
+                that._parent.animations.progress.classList.remove("length5min", "length10min");
+                that._parent.animations.main.classList.add("lengthinfinite");
+                that._parent.animations.progress.classList.add("lengthinfinite")
+                that._parent.animations.progressContainer.style.display = "none";
+                that.configPanel.querySelector('.set-length span.is-active').classList.remove("is-active");
+                btinfinite.classList.add("is-active");
+              }
+            });
+          }
+        }
+      }
+    },
+
+    sounds: {
+      in: null,
+      out: null,
+      _parent: null,
+
+      _load: function(caller) {
+        this._parent = caller;
+        this.in = new Howl({
+          src: ['/audio/in.wav']
+        });
+        this.out = new Howl({
+          src: ['/audio/out.wav']
+        });
+      },
+      _setupSoundTimers: function() {
+        var that = this;
+        var animationDuration = parseInt(window.getComputedStyle(that._parent.animations.main, "::before").animationDuration);
+        that._parent._runningTimer = new that._parent.setRecurringTimer(function () {
+            that.out.play();
+        }, animationDuration*1000, 0); // ex. 10s pour un cycle total => 5000ms pour le deuxième son
+        that._parent._runningTimer2 = new that._parent.setRecurringTimer(function () {
+            that.in.play();
+        }, animationDuration*1000, animationDuration*1000/2); // ex. 10s pour un cycle total => 5000ms pour le deuxième son
+        that._parent._runningTimer.pause();
+        that._parent._runningTimer2.pause();
+      },
+      play: function() {
+        this._parent._runningTimer.resume();
+        this._parent._runningTimer2.resume();
+      },
+
+      pause:function() {
+        this._parent._runningTimer.pause();
+        this._parent._runningTimer2.pause();
+      },
+
+      stop:function() {
+        this._parent._runningTimer.stop();
+        this._parent._runningTimer2.stop();
+      }
+      // _onIteration:function() {
+      //   var that = this,
+      //       animationDuration = parseInt(window.getComputedStyle(that.animations.main, "::before").animationDuration);
+      //
+      //   that.sounds.in.play();
+      //
+      //   that._runningTimer = new that.setTimer(function () {
+      //       that.sounds.out.play();
+      //   }, (animationDuration/2)*1000); // ex. 10s pour un cycle total => 5000ms pour le deuxième son
+      // }
     }
   }
 }
