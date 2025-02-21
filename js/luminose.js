27f43b0f9971f45b65d9a6c12501ecfdebb570dd
Florent Jaouali <florent.jaouali@gmail.com>
2025-02-21 09:45:15 +0000

    Update v5

--- js/luminose.js
+++ js/luminose.js
@@ -77,8 +77,10 @@ var App = {
       },
       urls: {
         calendly: {
-          adulte: 'https://calendly.com/luminose/seance-hypnose?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
-          enfant: 'https://calendly.com/luminose/seance-hypnose-enfant?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
+          premiereadulte: 'https://calendly.com/luminose/premiere-seance-adulte?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
+          premiereenfant: 'https://calendly.com/luminose/premiere-seance-enfant?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
+          adulte: 'https://calendly.com/luminose/seance-adulte?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
+          enfant: 'https://calendly.com/luminose/seance-enfant?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
           respiration: 'https://calendly.com/luminose/seance-respiration-holotropique?hide_gdpr_banner=1&hide_event_type_details=1&primary_color=6163a5',
         },
         rh: {
@@ -107,6 +109,7 @@ var App = {
     this.setupViewport();
     this.setupCookiesModal();
     this.setupNavigation();
+    this.setupCarouselTemoignages();
     this.setupButtonPriseRdv();
     this.setupSocialLinks();
     this.setupButtonConfirmationQuestionnaireSante();
@@ -304,6 +307,48 @@ var App = {
         });
       });
     }
+  },
+
+  setupCarouselTemoignages: function() {
+    const carouselContainer = document.querySelector(".temoignages.carousel-container");
+    if (carouselContainer === null) return;
+    const carousel = carouselContainer.querySelector(".carousel");
+    const temoignages = carousel.querySelectorAll(".temoignage");
+    const btnLeft = carouselContainer.querySelector(".button.left");
+    const btnRight = carouselContainer.querySelector(".button.right");
+    
+    const temoignagesAffiches = 3; // Nombre de témoignages visibles en même temps
+    const totalTemoignages = temoignages.length;
+    const maxIndex = Math.ceil(totalTemoignages / temoignagesAffiches) - 1; // Dernier index possible
+    const that = this;
+    let index = 0;
+
+    
+
+    btnRight.addEventListener("click", function () {
+      if (index < maxIndex) {
+        index++;
+        that._updateCarousel(carousel, btnLeft, btnRight, index, maxIndex);
+      }
+    });
+
+    btnLeft.addEventListener("click", function () {
+      if (index > 0) {
+        index--;
+        that._updateCarousel(carousel, btnLeft, btnRight, index, maxIndex);
+      }
+    });
+
+    that._updateCarousel(carousel, btnLeft, btnRight, index, maxIndex);
+  },
+
+  _updateCarousel:function(carousel, btnLeft, btnRight, index, maxIndex) {
+    const offset = -(index * 100) + "%";
+    carousel.style.transform = `translateX(${offset})`;
+
+    // Gérer l'état des boutons
+    btnLeft.disabled = index === 0;
+    btnRight.disabled = index === maxIndex;
   },
 
   setupButtonPriseRdv() {
