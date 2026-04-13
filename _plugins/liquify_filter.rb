91488e5771ffa991e661a7ea724d120ce4d9038a
Florent Jaouali <florent.jaouali@gmail.com>
2026-04-13 08:51:30 +0000

    Fix

---
+++ _plugins/liquify_filter.rb
@@ -0,0 +1,10 @@
+# _plugins/liquify_filter.rb
+module Jekyll
+  module LiquifyFilter
+    def liquify(input)
+      Liquid::Template.parse(input).render(@context)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::LiquifyFilter)
